971eb8bc2faa60718e65cdea88a68648a65a21cb
dhernandez6022 <dhernandez23@ggc.edu>
2025-10-29 22:33:49 -0400

    worked on capital and user class, still need to work on user

--- src/User.java
+++ src/User.java
@@ -1,3 +1,31 @@
 public class User {
+    private final String name;
+    private int correct = 0;
+    private int wrong=0;
+
+    public User(String name){
+        this.name= name;
+    }
+    //getters
+    public String getName(){
+        return name;
+    }
+    public int getCorrect(){
+        return correct;
+    }
+    public int getWrong(){
+        return wrong;
+    }
+    public int getTotal(){
+        return correct + wrong;
+    }
+    //add results
+    public void incCorrect(){
+        correct++;
+    }
+    public void incWrong(){
+        wrong++;
+    }
+    // get percentage for results
 
 }
